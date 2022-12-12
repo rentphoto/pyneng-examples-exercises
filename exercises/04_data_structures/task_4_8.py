561d8656876e25955ba2143d7101af0f05be3b28
python <python@debian>
2022-12-12 11:18:26 -0600

    221212

--- exercises/04_data_structures/task_4_8.py
+++ exercises/04_data_structures/task_4_8.py
@@ -26,3 +26,9 @@
 """
 
 ip = "192.168.3.1"
+a = ip.split('.')
+ip_template = '''
+{0:<8}  {1:<8}  {2:<8}  {3:<8}
+{0:08b}  {1:08b}  {2:08b}  {3:08b}
+'''
+print(ip_template.format(int(a[0]), int(a[1]), int(a[2]), int(a[3])))
