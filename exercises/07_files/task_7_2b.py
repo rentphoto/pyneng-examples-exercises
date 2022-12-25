c192b3a8c44fd7a607792d6f54422e6bd3e65951
python <python@debian>
2022-12-24 23:05:26 -0600

    221225

--- exercises/07_files/task_7_2b.py
+++ exercises/07_files/task_7_2b.py
@@ -16,4 +16,15 @@
 
 """
 
+from sys import argv
+
 ignore = ["duplex", "alias", "configuration"]
+
+filename = argv[1]
+
+with open(filename) as f:
+    for line in f:
+        words = line.split()
+        words_intersect = set(words) & set(ignore)
+        if not line.startswith("!") and not words_intersect:
+            print(line.rstrip())
